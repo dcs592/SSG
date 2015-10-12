da155a72f2be8bb0cbc8d95e667d3dc6ff0296db
Dane Stier <DaneStier@u.northwestern.edu>
2015-10-12 13:17:47 -0500

    Split pages; Facebook

--- ssg.js
+++ ssg.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 		document.getElementById("circle").style.marginLeft = 'auto';
 		document.getElementById("circle").style.marginRight = 'auto';
 		document.getElementById("circle").style.position = 'relative';
-		document.getElementById('circle').style.marginTop = '275px';
+		document.getElementById('circle').style.marginTop = '70px';
 
 		document.getElementById('upperleft').style.width = '90%';
 		document.getElementById('upperleft').style.marginLeft = '5%';
@@ -32,29 +32,36 @@ $(document).ready(function() {
 		document.getElementById("menu").style.position = "absolute";
 
 		document.getElementById('section1').style.width = '100%';
-		document.getElementById('schedule').style.width = '100%';
+	//	document.getElementById('schedule').style.width = '100%';
 		document.getElementById('section2').style.width = '100%';
 		document.getElementById('section2').style.position = 'relative';
-		document.getElementById('section3').style.width = '100%';
-		document.getElementById('section3').style.position = 'relative';
+	//	document.getElementById('section3').style.width = '100%';
+	//	document.getElementById('section3').style.position = 'relative';
 		document.getElementById('divider2').style.display = 'inline-block';
-		document.getElementById('divider3').style.display = 'inline-block';
+	//	document.getElementById('divider3').style.display = 'inline-block';
 		document.getElementById('section2').style.marginTop = '-30px';
-		document.getElementById('section3').style.marginTop = '-30px';
+	//	document.getElementById('section3').style.marginTop = '-30px';
 
 		document.getElementById('newstable1').style.width = '100%';
 		document.getElementById('newstable2').style.width = '100%';
 
-		document.getElementById('description').style.fontSize = "medium";
-		document.getElementById('description').style.width = '90%';
-		document.getElementById('descimage').style.width = '90%';
-		document.getElementById('descimage').style.cssFloat = "left";
-		document.getElementById('descimage').style.marginLeft = "5%";
-		document.getElementById('songdesc').style.fontSize = "medium";
+	//	document.getElementById('description').style.fontSize = "medium";
+	//	document.getElementById('description').style.width = '90%';
+	//	document.getElementById('descimage').style.width = '90%';
+	//	document.getElementById('descimage').style.cssFloat = "left";
+	//	document.getElementById('descimage').style.marginLeft = "5%";
+	//	document.getElementById('songdesc').style.fontSize = "medium";
 
-		document.getElementById('image1').style.width = '90%';
+	//	document.getElementById('image1').style.width = '90%';
 
-		var images = document.getElementsByClassName("image");
+		document.getElementById('expand').style.display = 'inline-block';
+		var menu = document.getElementsByClassName("full");
+		var i;
+		for (i = 0; i < menu.length; i++) {
+			menu[i].style.display = "none";
+		}
+
+	/*	var images = document.getElementsByClassName("image");
 		var i;
 		for (i = 0; i < images.length; i++) {
 			images[i].style.width = "90%";
@@ -93,7 +100,7 @@ $(document).ready(function() {
 			d4[i].style.cssFloat = "left";
 			d4[i].style.marginLeft = "5%";
 			d4[i].style.marginTop = "0";
-		}
+		}*/
 	}
 	else {
 		widthcheck = 0;
@@ -107,7 +114,7 @@ function Resize() {
 			document.getElementById("circle").style.marginLeft = 'auto';
 			document.getElementById("circle").style.marginRight = 'auto';
 			document.getElementById("circle").style.position = 'relative';
-			document.getElementById('circle').style.marginTop = '275px';
+			document.getElementById('circle').style.marginTop = '70px';
 
 			document.getElementById('upperleft').style.width = '90%';
 			document.getElementById('upperleft').style.marginLeft = '5%';
@@ -128,20 +135,20 @@ function Resize() {
 			document.getElementById("menu").style.position = "absolute";
 
 			document.getElementById('section1').style.width = '100%';
-			document.getElementById('schedule').style.width = '100%';
+		//	document.getElementById('schedule').style.width = '100%';
 			document.getElementById('section2').style.width = '100%';
 			document.getElementById('section2').style.position = 'relative';
-			document.getElementById('section3').style.width = '100%';
-			document.getElementById('section3').style.position = 'relative';
+		//	document.getElementById('section3').style.width = '100%';
+		//	document.getElementById('section3').style.position = 'relative';
 			document.getElementById('divider2').style.display = 'inline-block';
-			document.getElementById('divider3').style.display = 'inline-block';
+		//	document.getElementById('divider3').style.display = 'inline-block';
 			document.getElementById('section2').style.marginTop = '-30px';
-			document.getElementById('section3').style.marginTop = '-30px';
+		//	document.getElementById('section3').style.marginTop = '-30px';
 
 			document.getElementById('newstable1').style.width = '100%';
 			document.getElementById('newstable2').style.width = '100%';
 
-			document.getElementById('description').style.fontSize = "medium";
+		/*	document.getElementById('description').style.fontSize = "medium";
 			document.getElementById('description').style.width = '90%';
 			document.getElementById('descimage').style.width = '90%';
 			document.getElementById('descimage').style.cssFloat = "left";
@@ -149,7 +156,14 @@ function Resize() {
 			document.getElementById('songdesc').style.fontSize = "medium";
 
 			document.getElementById('image1').style.width = '90%';
-
+*/
+			document.getElementById('expand').style.display = 'inline-block';
+			var menu = document.getElementsByClassName("full");
+			var i;
+			for (i = 0; i < menu.length; i++) {
+				menu[i].style.display = "none";
+			}
+/*
 			var images = document.getElementsByClassName("image");
 			var i;
 			for (i = 0; i < images.length; i++) {
@@ -187,7 +201,7 @@ function Resize() {
 			for (i = 0; i < d4.length; i++) {
 				d4[i].style.width = "90%";
 				d4[i].style.marginTop = "0";
-			}
+			}*/
 		}
 		widthcheck = 1;
     }
@@ -195,7 +209,7 @@ function Resize() {
     	if (widthcheck==1) {
 			document.getElementById("circle").style.marginLeft = '35%';
 			document.getElementById("circle").style.position = 'absolute';
-			document.getElementById('circle').style.marginTop = '175px';
+			document.getElementById('circle').style.marginTop = '150px';
 
 			document.getElementById('upperleft').style.width = '35%';
 			document.getElementById('upperleft').style.marginLeft = '8%';
@@ -208,27 +222,27 @@ function Resize() {
 			document.getElementById('bottomleft').style.width = '35%';
 			document.getElementById('bottomleft').style.marginLeft = '8%';
 
-			document.getElementById('upperright').style.marginTop = '8%';
-			document.getElementById('upperleft').style.marginTop = '8%';
+			document.getElementById('upperright').style.marginTop = '6%';
+			document.getElementById('upperleft').style.marginTop = '6%';
 			document.getElementById('bottomright').style.marginTop = '6%';
 			document.getElementById('bottomleft').style.marginTop = '6%';
 
 			document.getElementById("menu").style.position = "fixed";
 
 			document.getElementById('section1').style.width = '75%';
-			document.getElementById('schedule').style.width = '60%';
+		//	document.getElementById('schedule').style.width = '60%';
 			document.getElementById('section2').style.width = '25%';
 			document.getElementById('section2').style.position = 'absolute';
-			document.getElementById('section3').style.width = '40%';
-			document.getElementById('section3').style.position = 'absolute';
+		//	document.getElementById('section3').style.width = '40%';
+		//	document.getElementById('section3').style.position = 'absolute';
 			document.getElementById('divider2').style.display = 'none';
-			document.getElementById('divider3').style.display = 'none';
+		//	document.getElementById('divider3').style.display = 'none';
 			document.getElementById('section2').style.marginTop = '0';
-			document.getElementById('section3').style.marginTop = '0';
+		//	document.getElementById('section3').style.marginTop = '0';
 
 			document.getElementById('newstable1').style.width = '50%';
 			document.getElementById('newstable2').style.width = '50%';
-
+/*
 			document.getElementById('description').style.fontSize = "large";
 			document.getElementById('description').style.width = '63%';
 			document.getElementById('descimage').style.width = '25%';
@@ -237,7 +251,14 @@ function Resize() {
 			document.getElementById('songdesc').style.fontSize = "large";
 
 			document.getElementById('image1').style.width = '50%';
-
+*/
+			document.getElementById('expand').style.display = 'none';
+			var menu = document.getElementsByClassName("full");
+			var i;
+			for (i = 0; i < menu.length; i++) {
+				menu[i].style.display = "inline-block";
+			}
+/*
 			var images = document.getElementsByClassName("image");
 			var i;
 			for (i = 0; i < images.length; i++) {
@@ -276,7 +297,7 @@ function Resize() {
 				d4[i].style.cssFloat = "right";
 				d4[i].style.marginTop = "-85px";
 				d4[i].style.marginLeft = "0";
-			}
+			}*/
 		}
 		widthcheck = 0;
     }
